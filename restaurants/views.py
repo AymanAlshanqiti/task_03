3b929d096f32273febe9f9847ec9c71f5402041e
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-02-10 16:34:51 +0300

    Finish tast_03 of Django

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,21 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    	"my_list": [
+    		{
+    			"restaurant_name": "Hashi",
+    			"food_type": "Arabic Food",
+    		},
+    		{
+    			"restaurant_name": "Byato",
+    			"food_type": "Italian Food",
+    		},
+    		{
+    			"restaurant_name": "Makani",
+    			"food_type": "Indian Food",
+    		},
+    	
+    	],
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +28,11 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    	"my_object": 
+    		{
+    			"restaurant_name": "Byato",
+    			"food_type": "Italian Food",
+    		},
 
     }
     return render(request, 'detail.html', context)
